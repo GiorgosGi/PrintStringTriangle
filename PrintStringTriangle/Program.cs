3f8a54b680f27e869272c04d25b12be78c692390
Giorgos Gial <g.gialouris@outlook.com.gr>
2020-11-17 18:07:29 +0200

    Add project files.

---
+++ PrintStringTriangle/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintStringTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Write a string: ");
+            string text = Console.ReadLine();
+            Console.WriteLine();
+
+            int j = 1;
+            for (int i = text.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine(text.Substring(i, j));
+                j++;
+            }
+            Console.WriteLine();
+        }
+    }
+}
